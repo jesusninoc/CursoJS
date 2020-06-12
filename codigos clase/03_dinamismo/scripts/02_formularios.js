91712a0c1584827bea41fee741bc09912cd99fcb
develop <developandsys@gmail.com>
2020-06-12 12:20:21 +0200

    update

---
+++ codigos clase/03_dinamismo/scripts/02_formularios.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let editNombre = document.querySelector('#nombre');
+let editApellido = document.querySelector('#apellido');
+let editTelefono = document.querySelector('#telefono');
+
+
+
+function botonPulsado() {
+    console.log('botonPulsado');
+    console.log(editNombre.value);
+    console.log(editApellido.value);
+    console.log(editTelefono.value);
+
+}
